6015a6289b3a3374e7a5d6f50c1edff5b9bb6b55
Folajimmie <hayodele24@gmail.com>
2022-06-30 01:19:24 +0100

    recently updated portfolio

--- src/component/Portfolio/index.jsx
+++ src/component/Portfolio/index.jsx
@@ -16,17 +16,19 @@ const index = () => {
           Portfolio.map( ({id, image, title, github, page, stack}) =>{
             return(
             <div key={id} className="porfolio-item">
-          <img src={image} alt={title} />
-          <div className="overlay">
-            <div className="overlay-content">
-              <h3>{title}</h3>
-              <p>{stack}</p>
-              <div className="overlay-icon">
-              <a href={github} target="_blank" rel="noopener noreferrer"><FaGithub className='icon'/></a>
-              <a href={page} target="_blank" rel="noopener noreferrer"><MdOutlineOpenInNew className='icon'/></a>
+              <div className="item">
+                <img src={image} alt={title} />
+                <div className="overlay">
+                  <div className="overlay-content">
+                    <h3>{title}</h3>
+                    <p>{stack}</p>
+                    <div className="overlay-icon">
+                    <a href={github} target="_blank" rel="noopener noreferrer"><FaGithub className='icon'/></a>
+                    <a href={page} target="_blank" rel="noopener noreferrer"><MdOutlineOpenInNew className='icon'/></a>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
         </div>
         );
           })
